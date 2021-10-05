1f858f4b68d3e567761e34d79dba00b42ea5d8ed
NakushnovE <evgen85n@gmail.com>
2021-10-05 14:09:21 +0400

    1

---
+++ test/specs/taskOne.ts
@@ -0,0 +1,36 @@
+const assert = require('assert')
+
+describe('My Login application', () => {
+    it.skip('should login with valid credentials', async () => {
+        await browser.url(`https://testingcup.pgs-soft.com/`)       
+        //const title = await browser.getTitle()
+        
+        //assert.strictEqual(title,  "Welcome!")
+        expect(browser).toHaveTitle("Welcome")
+        //assert(title === "Google")
+
+        /*const btn1 = await $('.//div[@class="col-md-6"]/a')
+        btn1.click()
+        await browser.url(`https://testingcup.pgs-soft.com/task_1`)  
+        const zagolovok = await $('.//li[text() = "Zadanie 1"]')
+        assert(zagolovok === "Zadanie 1")*/
+        //assert.strictEqual(zagolovok, 'Zadanie 1')
+    })
+})
+
+    it('should', async () => {
+        await browser.url(`https://testingcup.pgs-soft.com/`)       
+        let btn1 = await $('.//div[@class="col-md-6"]/a')
+        btn1.click()
+        await browser.pause(5000)
+        //await expect(browser).toHaveUrl('https://testingcup.pgs-soft.com/task_1')
+        const sumProd = 
+        $('//h4[text()="Okulary"]/following-sibling::div[@class="input-group input-group-sm"]/input')
+        await sumProd.setValue(3)
+        const addProd = 
+        $('//h4[text()="Okulary"]/following-sibling::div[@class="input-group input-group-sm"]/span/button')
+        await addProd.click()
+        const order = $('//div[text() = "Twój koszyk"]')
+        //await browser.pause(10000)
+        await expect(order).toBeDisplayed()
+    })
